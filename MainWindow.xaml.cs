31004b697046bfbd765bb8bfa4aa4621f7ddf772
Пак Пак <tov.pak@gmail.com>
2021-02-18 10:43:38 +0300

    Готова вкладка акции

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,23 +31,143 @@ namespace Музыкальный_магазин_пластинок
         {
             InitializeComponent();
             LogOut(new object(), new RoutedEventArgs());
-            
+
             ///для теста
             LoginBox.Text = "1";
             PassworBox.Password = "1";
             Logon(new object(), new RoutedEventArgs());
-            var d = new DateTime(2020,12,1);
+            var d = new DateTime(2020, 12, 1);
             StartDate.SelectedDate = d;
             lbReportTypes.SelectedIndex = 1;
             EndDate.SelectedDate = DateTime.Now;
 
         }
 
-        private void ClearStatusBar(object sender, RoutedEventArgs e)
+        //private void ClearStatusBar(object sender, RoutedEventArgs e)
+        //{
+        //    StatusBar.Text = String.Empty;
+        //}
+
+        private void Found_CategoryItems(object sender, RoutedEventArgs e)
         {
-            StatusBar.Text = String.Empty;
+            try
+            {
+                if (cbCategoryTypes.SelectedIndex == -1)
+                {
+                    StatusBar.Text = "Выбирите категорию";
+                    return;
+                }
+                cbCategoryList.ItemsSource = null;
+                cbCategoryList.Items.Clear();
+                switch (cbCategoryTypes.Text)
+                {
+                    case "Исполнители":
+                        var result = магазин.Исполнители.Where<Исполнители>(s => s.Имя.Contains(tbSearchCategoryItems.Text));
+                        List<string> ComboItems = new List<string>();
+                        foreach (var res in result)
+                        {
+                            ComboItems.Add(res.Имя);
+                        }
+                        cbCategoryList.ItemsSource = ComboItems;
+                        break;
+                    case "Жанры":
+                        var result_1 = магазин.Жанры.Where<Жанры>(s => s.Название.Contains(tbSearchCategoryItems.Text));
+                        List<string> ComboItems_1 = new List<string>();
+                        foreach (var res in result_1)
+                        {
+                            ComboItems_1.Add(res.Название);
+                        }
+                        cbCategoryList.ItemsSource = ComboItems_1;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                StatusBar.Text = ex.Message;
+
+            }
         }
 
-        
+        private void Add_Sales(object sender, RoutedEventArgs e)
+        {
+            try {
+                if (cbCategoryTypes.SelectedIndex == -1)
+                {
+                    StatusBar.Text = "Выбирите категорию";
+                    return;
+                }
+                else if (cbCategoryList.SelectedIndex == -1)
+                {
+                    StatusBar.Text = "Выбирите наименование";
+                    return;
+                }
+                else if (cbSalesAmount.SelectedIndex == -1)
+                {
+                    StatusBar.Text = "Выбирите размер скидки";
+                    return;
+                }
+                switch (cbCategoryTypes.Text)
+                {
+                    case "Исполнители":
+                        var result = магазин.Пластинки.Where<Пластинки>(s => s.Исполнители.Имя.Contains(cbCategoryList.Text));
+                        foreach (var res in result)
+                        {
+                            int i = 0;
+                            Int32.TryParse(cbSalesAmount.Text, out i);
+                            res.Скидка = i;
+                        }
+                        магазин.SaveChanges();
+                        break;
+                    case "Жанры":
+                        var result_1 = магазин.Пластинки.Where<Пластинки>(s => s.Жанры.Название.Contains(cbCategoryList.Text));
+                        foreach (var res in result_1)
+                        {
+                            int i = 0;
+                            Int32.TryParse(cbSalesAmount.Text, out i);
+                            res.Скидка = i;
+                        }
+                        магазин.SaveChanges();
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                StatusBar.Text = ex.Message;
+            }
+            }
+
+        private void SearchSinglesToAddSales(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cbSearhingContentResult.ItemsSource = null;
+                cbSearhingContentResult.Items.Clear();
+                var result = магазин.Пластинки.Where<Пластинки>(s => s.Название.Contains(tbSearchText.Text));
+                List<string> ComboItems = new List<string>();
+                foreach (var res in result)
+                {
+                    ComboItems.Add(res.Название);
+                }
+                cbSearhingContentResult.ItemsSource = ComboItems;
+            }
+            catch (Exception ex)
+            {
+                StatusBar.Text = ex.Message;
+            }
+        }
+
+        private void ChooseSingleToAddSale(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var result = магазин.Пластинки.Where<Пластинки>(s => s.Название.Contains(cbSearhingContentResult.SelectedItem.ToString()));
+                gvSearchingResalt.ItemsSource = result.ToList();
+                EditColumns(gvSearchingResalt);
+            }
+            catch (Exception ex)
+            {
+                StatusBar.Text = ex.Message;
+            }
+        }
     }
 }
--- Методы формирования отчетов.cs
+++ Методы формирования отчетов.cs
@@ -25,110 +25,117 @@ namespace Музыкальный_магазин_пластинок
 
         private void CreateReport(object sender, RoutedEventArgs e)
         {
-            var Sales = магазин.Продажи.Where<Продажи>(S => S.Дата_продажи.Value.Year > StartDate.SelectedDate.Value.Year && S.Дата_продажи.Value.Year <= EndDate.SelectedDate.Value.Year);
-            switch (lbReportTypes.Text)
+            try
             {
-                case "Новинки":
-                    var Report = магазин.Пластинки.Where<Пластинки>(s => s.Год_издания > StartDate.SelectedDate.Value.Year && s.Год_издания < EndDate.SelectedDate.Value.Year);
-                    ReportView.ItemsSource = Report.ToList<Пластинки>();
-                    EditColumns(ReportView);
-                    break;
-                case "Самые продаваемые пластинки":
-                    var sale = from Продажи in магазин.Set<Продажи>()
-                               join Пластинки in магазин.Set<Пластинки>()
-                               on Продажи.ID_пластинки equals Пластинки.Id
-                               join Исполнители in магазин.Set<Исполнители>()
-                               on Пластинки.Испольнитель_ID equals Исполнители.Id
-                               select new TopSinglesReport { Название = Пластинки.Название, Исполнитель = Исполнители.Имя, Всего_продано = (int)Продажи.Количество };
-                    var s1 = from p in sale
-                             group p by p.Название into g
-                             select g;
-                    List<TopSinglesReport> GroupingTopReport = new List<TopSinglesReport>();
-                    foreach (var Group in s1)
-                    {
+                var Sales = магазин.Продажи.Where<Продажи>(S => S.Дата_продажи.Value.Year > StartDate.SelectedDate.Value.Year && S.Дата_продажи.Value.Year <= EndDate.SelectedDate.Value.Year);
+                switch (lbReportTypes.Text)
+                {
+                    case "Новинки":
+                        var Report = магазин.Пластинки.Where<Пластинки>(s => s.Год_издания > StartDate.SelectedDate.Value.Year && s.Год_издания < EndDate.SelectedDate.Value.Year);
+                        ReportView.ItemsSource = Report.ToList<Пластинки>();
+                        EditColumns(ReportView);
+                        break;
+                    case "Самые продаваемые пластинки":
+                        var sale = from Продажи in магазин.Set<Продажи>()
+                                   join Пластинки in магазин.Set<Пластинки>()
+                                   on Продажи.ID_пластинки equals Пластинки.Id
+                                   join Исполнители in магазин.Set<Исполнители>()
+                                   on Пластинки.Испольнитель_ID equals Исполнители.Id
+                                   select new TopSinglesReport { Название = Пластинки.Название, Исполнитель = Исполнители.Имя, Всего_продано = (int)Продажи.Количество };
+                        var s1 = from p in sale
+                                 group p by p.Название into g
+                                 select g;
+                        List<TopSinglesReport> GroupingTopReport = new List<TopSinglesReport>();
+                        foreach (var Group in s1)
+                        {
 
-                        foreach (var grouping in Group)
-                            if (GroupingTopReport.Exists(ReportItem => ReportItem.Название == grouping.Название))
-                            {
-                                GroupingTopReport.Find(Item => Item.Название == grouping.Название).Всего_продано += grouping.Всего_продано;
-                            }
-                            else
-                                GroupingTopReport.Add(grouping);
-                    }
-                    if (GroupingTopReport.Count > 10)
-                    {
-                        GroupingTopReport.RemoveRange(9, GroupingTopReport.Count - 10);
-                    }
-                    GroupingTopReport.Sort(SortReport);
-                    ReportView.ItemsSource = GroupingTopReport;
-                    ReportView.Columns[2].Header = "Всего продано";
-                    break;
-                case "Самые популярные авторы":
+                            foreach (var grouping in Group)
+                                if (GroupingTopReport.Exists(ReportItem => ReportItem.Название == grouping.Название))
+                                {
+                                    GroupingTopReport.Find(Item => Item.Название == grouping.Название).Всего_продано += grouping.Всего_продано;
+                                }
+                                else
+                                    GroupingTopReport.Add(grouping);
+                        }
+                        if (GroupingTopReport.Count > 10)
+                        {
+                            GroupingTopReport.RemoveRange(9, GroupingTopReport.Count - 10);
+                        }
+                        GroupingTopReport.Sort(SortReport);
+                        ReportView.ItemsSource = GroupingTopReport;
+                        ReportView.Columns[2].Header = "Всего продано";
+                        break;
+                    case "Самые популярные авторы":
 
-                    var sales = from Продажи in магазин.Set<Продажи>()
-                                join Пластинки in магазин.Set<Пластинки>()
-                                on Продажи.ID_пластинки equals Пластинки.Id
-                                join Исполнители in магазин.Set<Исполнители>()
-                                on Пластинки.Испольнитель_ID equals Исполнители.Id
-                                select new TopSinglesReport { Название = Пластинки.Название, Исполнитель = Исполнители.Имя, Всего_продано = (int)Продажи.Количество };
-                    var s2 = from p in sales
-                             group p by p.Исполнитель into g
-                             select g;
-                    List<TopSinglesReport> GroupingTopReport_2 = new List<TopSinglesReport>();
-                    foreach (var Group in s2)
-                    {
+                        var sales = from Продажи in магазин.Set<Продажи>()
+                                    join Пластинки in магазин.Set<Пластинки>()
+                                    on Продажи.ID_пластинки equals Пластинки.Id
+                                    join Исполнители in магазин.Set<Исполнители>()
+                                    on Пластинки.Испольнитель_ID equals Исполнители.Id
+                                    select new TopSinglesReport { Название = Пластинки.Название, Исполнитель = Исполнители.Имя, Всего_продано = (int)Продажи.Количество };
+                        var s2 = from p in sales
+                                 group p by p.Исполнитель into g
+                                 select g;
+                        List<TopSinglesReport> GroupingTopReport_2 = new List<TopSinglesReport>();
+                        foreach (var Group in s2)
+                        {
 
-                        foreach (var grouping in Group)
-                            if (GroupingTopReport_2.Exists(ReportItem => ReportItem.Название == grouping.Название))
-                            {
-                                GroupingTopReport_2.Find(Item => Item.Название == grouping.Название).Всего_продано += grouping.Всего_продано;
-                            }
-                            else
-                                GroupingTopReport_2.Add(grouping);
-                    }
-                    if (GroupingTopReport_2.Count > 10)
-                    {
-                        GroupingTopReport_2.RemoveRange(9, GroupingTopReport_2.Count - 10);
-                    }
-                    GroupingTopReport_2.Sort(SortReport);
-                    ReportView.ItemsSource = GroupingTopReport_2;
-                    ReportView.Columns[2].Header = "Всего продано";
-                    ReportView.Columns.Remove(ReportView.Columns.Where(s => s.Header.Equals("Название")).FirstOrDefault());
-                    break;
-                case "Самые популярные жанры":
-                    var sales_1 = from Продажи in магазин.Set<Продажи>()
-                                  join Пластинки in магазин.Set<Пластинки>()
-                                  on Продажи.ID_пластинки equals Пластинки.Id
-                                  join Жанры in магазин.Set<Жанры>()
-                                  on Пластинки.Жанр_ID equals Жанры.Id
-                                  join Исполнители in магазин.Set<Исполнители>()
-                                 on Пластинки.Испольнитель_ID equals Исполнители.Id
-                                  select new TopSinglesReport { Название = Жанры.Название, Исполнитель = Исполнители.Имя, Всего_продано = (int)Продажи.Количество };
-                    var s3 = from p in sales_1
-                             group p by p.Название into g
-                             select g;
-                    List<TopSinglesReport> GroupingTopReport_3 = new List<TopSinglesReport>();
-                    foreach (var Group in s3)
-                    {
+                            foreach (var grouping in Group)
+                                if (GroupingTopReport_2.Exists(ReportItem => ReportItem.Название == grouping.Название))
+                                {
+                                    GroupingTopReport_2.Find(Item => Item.Название == grouping.Название).Всего_продано += grouping.Всего_продано;
+                                }
+                                else
+                                    GroupingTopReport_2.Add(grouping);
+                        }
+                        if (GroupingTopReport_2.Count > 10)
+                        {
+                            GroupingTopReport_2.RemoveRange(9, GroupingTopReport_2.Count - 10);
+                        }
+                        GroupingTopReport_2.Sort(SortReport);
+                        ReportView.ItemsSource = GroupingTopReport_2;
+                        ReportView.Columns[2].Header = "Всего продано";
+                        ReportView.Columns.Remove(ReportView.Columns.Where(s => s.Header.Equals("Название")).FirstOrDefault());
+                        break;
+                    case "Самые популярные жанры":
+                        var sales_1 = from Продажи in магазин.Set<Продажи>()
+                                      join Пластинки in магазин.Set<Пластинки>()
+                                      on Продажи.ID_пластинки equals Пластинки.Id
+                                      join Жанры in магазин.Set<Жанры>()
+                                      on Пластинки.Жанр_ID equals Жанры.Id
+                                      join Исполнители in магазин.Set<Исполнители>()
+                                     on Пластинки.Испольнитель_ID equals Исполнители.Id
+                                      select new TopSinglesReport { Название = Жанры.Название, Исполнитель = Исполнители.Имя, Всего_продано = (int)Продажи.Количество };
+                        var s3 = from p in sales_1
+                                 group p by p.Название into g
+                                 select g;
+                        List<TopSinglesReport> GroupingTopReport_3 = new List<TopSinglesReport>();
+                        foreach (var Group in s3)
+                        {
 
-                        foreach (var grouping in Group)
-                            if (GroupingTopReport_3.Exists(ReportItem => ReportItem.Название == grouping.Название))
-                            {
-                                GroupingTopReport_3.Find(Item => Item.Название == grouping.Название).Всего_продано += grouping.Всего_продано;
-                            }
-                            else
-                                GroupingTopReport_3.Add(grouping);
-                    }
-                    if (GroupingTopReport_3.Count > 10)
-                    {
-                        GroupingTopReport_3.RemoveRange(9, GroupingTopReport_3.Count - 10);
-                    }
-                    GroupingTopReport_3.Sort(SortReport);
-                    ReportView.ItemsSource = GroupingTopReport_3;
-                    ReportView.Columns[2].Header = "Всего продано";
-                    ReportView.Columns[0].Header = "Жанр";
-                    ReportView.Columns.Remove(ReportView.Columns.Where(s => s.Header.Equals("Исполнитель")).FirstOrDefault());
-                    break;
+                            foreach (var grouping in Group)
+                                if (GroupingTopReport_3.Exists(ReportItem => ReportItem.Название == grouping.Название))
+                                {
+                                    GroupingTopReport_3.Find(Item => Item.Название == grouping.Название).Всего_продано += grouping.Всего_продано;
+                                }
+                                else
+                                    GroupingTopReport_3.Add(grouping);
+                        }
+                        if (GroupingTopReport_3.Count > 10)
+                        {
+                            GroupingTopReport_3.RemoveRange(9, GroupingTopReport_3.Count - 10);
+                        }
+                        GroupingTopReport_3.Sort(SortReport);
+                        ReportView.ItemsSource = GroupingTopReport_3;
+                        ReportView.Columns[2].Header = "Всего продано";
+                        ReportView.Columns[0].Header = "Жанр";
+                        ReportView.Columns.Remove(ReportView.Columns.Where(s => s.Header.Equals("Исполнитель")).FirstOrDefault());
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                StatusBar.Text = "Ошибка" + ex.Message;
             }
         }
 
@@ -142,7 +149,7 @@ namespace Музыкальный_магазин_пластинок
             report.Columns.Where(s => s.Header.Equals("Год_издания")).FirstOrDefault().Header = "Год издания";
             report.Columns.Remove(report.Columns.Where(s => s.Header.Equals("Id")).FirstOrDefault());
             report.Columns.Remove(report.Columns.Where(s => s.Header.Equals("Себестоимость")).FirstOrDefault());
-            report.Columns.Remove(report.Columns.Where(s => s.Header.Equals("Скидка")).FirstOrDefault());
+            //report.Columns.Remove(report.Columns.Where(s => s.Header.Equals("Скидка")).FirstOrDefault());
             report.Columns.Remove(report.Columns.Where(s => s.Header.Equals("ID_зарезервировавшего")).FirstOrDefault());
             report.Columns.Remove(report.Columns.Where(s => s.Header.Equals("Испольнитель_ID")).FirstOrDefault());
             report.Columns.Remove(report.Columns.Where(s => s.Header.Equals("Издатель_ID")).FirstOrDefault());
